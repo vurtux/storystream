d29b743b6bb9aeedfa911b5578aa035e0fa71a7a
Amit Yadav <amit.yadav@ens.enterprises>
2025-10-08 12:33:15 +0530

    wrap Home client in Suspense

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,11 @@
-// ✅ /app/home/page.tsx
+// /app/home/page.tsx
 import HomeClient from "../../components/dashboard/home/HomeClient";
+import { Suspense } from "react";
 
 export default function Page() {
-  return <HomeClient />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <HomeClient />
+    </Suspense>
+  );
 }
